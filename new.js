e3108cf647a2f9a063fc235a3c92aefc3fa3ffac
abedattal98 <abed_attal@hotmail.com>
2021-07-21 12:56:56 +0300

    change file

--- new.js
+++ new.js
@@ -1,11 +1,25 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// // const { MongoClient } = require('mongodb');
+// // const client = new MongoClient(uri);
+
+// // create express app
+// const app = express();
+
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+// // parse application/json
+// app.use(bodyParser.json())
 const { MongoClient } = require('mongodb');
 
 async function main() {
 
     const uri = "mongodb+srv://abedattal98:w18w4KYFWDjv9gJk@cluster0.enft9.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
+    const dbConfig = require('./config/database.config.js');
 
 
-    const client = new MongoClient(uri);
+    const client = new MongoClient(dbConfig.url);
 
     try {
         // Connect to the MongoDB cluster
@@ -21,9 +35,19 @@ async function main() {
         await client.close();
     }
 }
+// main().catch(console.error);
 
+// app.get('/', (req, res) => {
+//     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+// });
+
+// require('./app/routes/note.routes.js')(app);
+
+// // listen for requests
+// app.listen(3000, () => {
+//     console.log("Server is listening on port 3000");
+// });
 main().catch(console.error);
-
 
 async function listDatabases(client) {
     databasesList = await client.db().admin().listDatabases();
--- server.js
+++ server.js
@@ -20,9 +20,12 @@ mongoose.Promise = global.Promise;
 
 // Connecting to the database
 mongoose.connect(dbConfig.url, {
-	useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 }).then(() => {
-    console.log("Successfully connected to the database");    
+    console.log("Successfully connected to the database");
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
@@ -30,7 +33,7 @@ mongoose.connect(dbConfig.url, {
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    res.json({ "message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes." });
 });
 
 require('./app/routes/note.routes.js')(app);
