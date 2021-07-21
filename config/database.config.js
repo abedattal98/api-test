9eab2e934a900fe0319d8db7242db9d70d8479c4
abedattal98 <abed_attal@hotmail.com>
2021-07-21 10:29:43 +0300

    update

---
+++ config/database.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: 'mongodb+srv://abedattal98:w18w4KYFWDjv9gJk@cluster0.enft9.mongodb.net/sample_airbnb?retryWrites=true&w=majority'
+}
--- mongo.js
+++ mongo.js
@@ -5,8 +5,8 @@ async function main() {
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/sample_airbnb?retryWrites=true&w=majority";
-    
+    const uri = "mongodb+srv://abedattal98:w18w4KYFWDjv9gJk@cluster0.enft9.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
+
     /**
      * The Mongo Client you will use to interact with your database
      * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
@@ -19,8 +19,46 @@ async function main() {
     try {
         // Connect to the MongoDB cluster
         await client.connect();
-
+        await listDatabases(client);
+        await createListing(client,
+            {
+                name: "Lovely Loft",
+                summary: "A charming loft in Paris",
+                bedrooms: 1,
+                bathrooms: 1
+            }
+        );
+        await createMultipleListings(client, [
+            {
+                name: "Infinite Views",
+                summary: "Modern home with infinite views from the infinity pool",
+                property_type: "House",
+                bedrooms: 5,
+                bathrooms: 4.5,
+                beds: 5
+            },
+            {
+                name: "Private room in London",
+                property_type: "Apartment",
+                bedrooms: 1,
+                bathroom: 1
+            },
+            {
+                name: "Beautiful Beach House",
+                summary: "Enjoy relaxed beach living in this house with a private beach",
+                bedrooms: 4,
+                bathrooms: 2.5,
+                beds: 7,
+                last_review: new Date()
+            }
+        ]);
+        await findOneListingByName(client, "Infinite Views");
+        await updateListingByName(client, "Infinite Views", { bedrooms: 6, beds: 8 });
+        await deleteListingByName(client, "Cozy Cottage");
+        await deleteListingsScrapedBeforeDate(client, new Date("2019-02-15"));
         // Make the appropriate DB calls
+    } catch (e) {
+        console.error(e);
 
     } finally {
         // Close the connection to the MongoDB cluster
@@ -29,3 +67,46 @@ async function main() {
 }
 
 main().catch(console.error);
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function createListing(client, newListing){
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+async function createMultipleListings(client, newListings){
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertMany(newListings);
+    console.log(`${result.insertedCount} new listing(s) created with the following id(s):`);
+    console.log(result.insertedIds);
+}
+async function findOneListingByName(client, nameOfListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").findOne({ name: nameOfListing });
+    if (result) {
+        console.log(`Found a listing in the collection with the name '${nameOfListing}':`);
+        console.log(result);
+    } else {
+        console.log(`No listings found with the name '${nameOfListing}'`);
+    }
+}
+async function updateListingByName(client, nameOfListing, updatedListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
+                        .updateOne({ name: nameOfListing }, { $set: updatedListing });
+
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+async function deleteListingByName(client, nameOfListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
+            .deleteOne({ name: nameOfListing });
+    console.log(`${result.deletedCount} document(s) was/were deleted.`);
+}
+async function deleteListingsScrapedBeforeDate(client, date) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
+        .deleteMany({ "last_scraped": { $lt: date } });
+    console.log(`${result.deletedCount} document(s) was/were deleted.`);
+}
